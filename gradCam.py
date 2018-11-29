37c9cb25a03cd8408c37e90901f0227c842e98ff
Menua <gmenua@gmail.com>
2018-11-29 15:21:47 +0400

    +

--- gradCam.py
+++ gradCam.py
@@ -25,14 +25,15 @@ import numpy as np
 def gradCamToHeatMap(cam,im):
     heatShape = im.shape[:2]
     heatmap = cv2.resize(cam[0],heatShape)
-    colored = 0.3*im+0.7*cv2.applyColorMap(np.uint8(255*heatmap), cv2.COLORMAP_JET)
+    colored = np.uint8(0.7*im+0.3*cv2.applyColorMap(np.uint8(255*heatmap), cv2.COLORMAP_JET))
     return heatmap, colored
 
 if __name__ == "__main__":
 
     from networks import getNetwork
     import matplotlib.pyplot as plt
-
+    from streamReader import StreamReader
+    import itertools
     sess = tf.Session()
     tf.keras.backend.set_session(sess)
     nn,ph = getNetwork(name="VGG16")
@@ -40,12 +41,23 @@ if __name__ == "__main__":
     preSoftMax = nn.output.op.inputs[0]
     lastConv = nn.layers[-6 ]
     A = lastConv.output
-    preSoftMax,A
+
     gradCamT = gradCam(preSoftMax,A)
     im = cv2.imread('sample_images/ManCoffee.jpeg')
     im = cv2.imread('sample_images/cat_dog.png')
     im = cv2.resize(im,(224,224))
+    # for i in range(1000):
     res = sess.run(gradCamT,{ph:[im]})
     heatmap,colored = gradCamToHeatMap(res,im)
 
     cv2.imwrite("gradcam.jpg",colored)
+    with StreamReader("http://192.168.16.101:8081/video") as cap:
+
+        for frame,num in zip(cap.read(),itertools.count()):
+            im = cv2.resize(frame,(224,224))
+            res = sess.run(gradCamT,{ph:[im]})
+            heatmap,colored = gradCamToHeatMap(res,im)
+            cv2.imshow("cam",colored)
+            cv2.imwrite(f"cams/{num}.jpeg",colored)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
