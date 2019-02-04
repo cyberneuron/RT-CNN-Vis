f30042affa8f0bf550443c603b459f8e893e07f1
Menua <gmenua@gmail.com>
2019-02-04 19:15:49 +0400

    support for networks with  unknown shapes of input images

--- maps.py
+++ maps.py
@@ -7,17 +7,10 @@ def mapsToGrid(output):
     numColumns = math.ceil(numMaps**0.5)
     numRows = math.ceil(numMaps/numColumns)
     zerosNum = numRows*numColumns-numMaps
-    zerosShape = [int(i) for i in layer.shape]
-    zerosShape[-1] = zerosNum
-    zeros = tf.zeros(
-        zerosShape,
-        dtype=tf.float32,
-        name=None)
-    concated = tf.concat([layer,zeros],-1)
-    len,width,depth= [s for s in concated.shape]
-    mapStack =tf.unstack(concated,axis=2)
+    mapStack =tf.unstack(layer,axis=2)
+    map_on_tail = mapStack[-1]
+    for i in range(zerosNum):
+        mapStack.append(map_on_tail)
     rowStacks = [tf.concat(mapStack[i:i+numColumns],axis=1) for i in range(0,numColumns*numRows,numColumns)]
     result = tf.concat(rowStacks,axis=0)
     return result,(numColumns,numRows), mapStack
-    # reshaped = tf.reshape(concated,(len*numColumns,width*numRows))
-    # return reshaped
